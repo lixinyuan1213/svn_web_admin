04fe4c3e8e9ddccccd549d78c425407b01584754
lisy <768104762@qq.com>
2019-02-24 17:10:49 +0800

    注释

--- go/config/Config.go
+++ go/config/Config.go
@@ -7,11 +7,17 @@ import (
 )
 
 type Config struct {
+	//服务端口
 	Port string `json:"port"`
+	//项目根路径
 	Path string `json:"path"`
+	//svn根路径
 	SvnPath string `json:"svn_root_path"`
+	//管理员登录账号
 	AdminName string `json:"admin"`
+	//管理员登录密码
 	Password string `json:"password"`
+	//删除svn项目时的密码
 	DelPwd string `json:"del_pwd"`
 }
 func GetConfig()  Config{
